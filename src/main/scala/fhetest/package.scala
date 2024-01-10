d06ebc6966891a71e485cb2052beb2921a195c07
jaeho <Maokami>
2024-01-10 02:19:54 +0000

    Import some T2 programs and expected result from T2 project (#5)

--- src/main/scala/fhetest/package.scala
+++ src/main/scala/fhetest/package.scala
@@ -6,5 +6,15 @@ val BASE_DIR = settings.ProjectSettings.projectHome
 /** log directory */
 val LOG_DIR = s"$BASE_DIR/logs"
 
-/* T2 directory */
+/** T2 directory */
 val T2_DIR = s"$BASE_DIR/src/main/java/T2-FHE-Compiler-and-Benchmarks"
+
+/** resource directory */
+val RESOURCE_DIR = s"$BASE_DIR/src/main/resources"
+val T2_RESOURCE_DIR = s"$RESOURCE_DIR/t2"
+val RESULT_RESOURCE_DIR = s"$RESOURCE_DIR/result"
+
+/* Workspace directory */
+val WORKSPACE_DIR = s"$BASE_DIR/workspace"
+val SEAL_DIR = s"$WORKSPACE_DIR/SEAL"
+val OPENFHE_DIR = s"$WORKSPACE_DIR/OpenFHE"
