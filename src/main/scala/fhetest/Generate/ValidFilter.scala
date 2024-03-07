851019ed1aa41ec84aff8474948bdfba949dca16
Hyerin Park <hyerin.park@kaist.ac.kr>
2024-03-07 05:25:57 +0000

    ValidFilter: fix minor

--- src/main/scala/fhetest/Generate/ValidFilter.scala
+++ src/main/scala/fhetest/Generate/ValidFilter.scala
@@ -16,7 +16,7 @@ def ringDimIsPowerOfTwo(n: Int): Boolean = (n > 0) && ((n & (n - 1)) == 0)
 
 def plainModIsPositive(m: Int): Boolean = m > 0
 
-def plainModEnableBatching(m: Int, n: Int): Boolean = (m % n) == 1
+def plainModEnableBatching(m: Int, n: Int): Boolean = (m % (2 * n)) == 1
 
 def lenIsLessThanRingDim(len: Int, n: Int, scheme: Scheme): Boolean =
   scheme match {
